4b528f5ee3ef8bb58e1815961a616af224b7ddba
BlackZ <jpoeppel@techfak.uni-bielefeld.de>
2015-02-16 15:17:05 +0100

    added the testfile for the logParser

---
+++ src/logParser.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  16 15:7:52 2015
+
+Module containing the parser object handling the parsing of the logfiles.
+@author: jpoeppel
+"""
---
+++ src/tests/test_logParser.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  16 15:7:52 2015
+Testcase for the logParser.
+@author: jpoeppel
+"""
+
+import unittest
+import logParser
+
+
+class TestParserObject(unittest.TestCase):
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()
