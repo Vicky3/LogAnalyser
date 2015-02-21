3d9c645e655ef8f92f114c08263c3ab3cf0c9b73
BlackZ <jpoeppel@techfak.uni-bielefeld.de>
2015-02-21 23:46:58 +0100

    sort keys human like for barchars

--- src/svgCreator.py
+++ src/svgCreator.py
@@ -11,6 +11,13 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import operator
 import StringIO
+import time
+import re
+
+NUM_RE = re.compile('([0-9]+)')
+def ak4(s):
+    s = s[0]
+    return [ int(c) if c.isdigit() else c for c in NUM_RE.split(s) ]
 
 def createBarChart(data,top=None):
     """
@@ -32,25 +39,41 @@ def createBarChart(data,top=None):
     #top too big?
     if top>=len(data.values()):
         top=None
+        
+    usedDates = False
+    try:
+        sorted_x = [(time.strptime(k, "%d/%b/%Y"), v) for k,v in data.items()]
+    except ValueError:
+        usedDates = False
+        sorted_x = data.items()
+    else:
+        usedDates = True
     #top given
     if top:
         top-=1#number of bars from data
         #sort dictionary in lists to melt the smallest bars together
-        sorted_x = sorted(data.items(), key=operator.itemgetter(1),
+        sorted_x = sorted(sorted_x, key=operator.itemgetter(1),
                           reverse=True)
         topSorted = sorted_x[:top]
         topSorted.append(('other', sum(n for _, n in sorted_x[top:])))
     #no top given - use full data
     else:
-        topSorted = data.items()
+        topSorted = sorted_x
 
-    #sort by keys (e.g. alphabetically)
-    sorted_x = sorted(topSorted, key=operator.itemgetter(0),
+    #sort by keys (e.g. alphabetically) Differentiate between dates and no dates
+    if usedDates:
+        sorted_x = sorted(topSorted, key=operator.itemgetter(0), 
+                          reverse=False)
+    else:
+        sorted_x = sorted(topSorted, key=ak4,
                           reverse=False)
     l,x= map(list, zip(*sorted_x))
 
     #Convert values
-    l=[s.encode('utf-8').decode('utf-8') for s in l]
+    if usedDates:
+        l=[time.strftime("%d/%b/%Y",s).encode('utf-8').decode('utf-8') for s in l]
+    else:
+        l=[s.encode('utf-8').decode('utf-8') for s in l]
     #build svg
     xpos=range(len(x))#pos of left bottom edge of bars
     xlpos=[n+0.4 for n in xpos]#pos of labels
